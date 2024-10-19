b41c46415b0941755ce7be1431c984e6b21098eb
arritxu <arritxums@gmail.com>
2024-10-19 23:36:09 +0200

    commit Arritxu2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola subo este mensaje Arritxu1");
+        System.out.println("Hola Arritxu1, soy arritxu2");
+
       
 
     }
