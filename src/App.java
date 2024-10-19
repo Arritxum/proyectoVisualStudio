31d9e32a3afee74e235ed8b6c828797fc290ff79
Arritokieta Mateos <arritxums@gmail.com>
2024-10-19 23:45:15 +0200

    commit bye

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Agur Arritxu1 yo tambien");
+        System.out.println("bye");
 
       
 
