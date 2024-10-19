19d993850ab3ac5c95642d9a45ecef45503d5226
Arritokieta Mateos <arritxums@gmail.com>
2024-10-19 23:32:34 +0200

    commit arritxu1

--- src/App.java
+++ src/App.java
@@ -1,11 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World2!");
+        System.out.println("Hola subo este mensaje Arritxu1");
+      
 
     }
-    public static void metodo1() {
-        System.out.println("Hello, World!");
-    }
+   
     
 }
